c8c3503e8d39f72d54f2fcce1e112653cbafbd63
nabin <nabin043@gmail.com>
2023-05-18 17:40:04 +0545

    fix-std-get-admin-issue

--- resolvers.js
+++ resolvers.js
@@ -333,8 +333,7 @@ const resolvers = {
                 for (let index = 0; index < student.length; index++) {
                     const course = await prisma.jmkcrsinfo.findFirst({ where: { crs_id: student[index].crs_id } })
                     if (course) {
-                        const mergestudent = student.map(i => ({ ...i, crs_type: course.crs_type, crs_name: course.crs_name }))
-                        students.push(...mergestudent)
+                        students.push({ ...student[index], crs_type: course.crs_type, crs_name: course.crs_name })
                     }
                 }
                 return students;
