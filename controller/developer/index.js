cc1555135200ec4707683f1f4326196536c58ef3
Suhant <72059795+Wolly-code@users.noreply.github.com>
2023-05-25 20:28:31 +0545

    Added the following - ProjectList  - ExperienceList - Create Experience - Update Experience

--- controller/developer/index.js
+++ controller/developer/index.js
@@ -1,7 +1,6 @@
 import prisma from "../../database.js";
 import jwt from 'jsonwebtoken';
 import { ApolloError, AuthenticationError, ForbiddenError } from 'apollo-server-express';
-import { uploadImgToAWS } from "../../utils/imageHandler.js";
 
 const ROLE = "Developer"
 
@@ -33,41 +32,27 @@ const developerQueryTypesAndInputs = `
         experience: Int!
         projects:Int!
     }
+    type Experience{
+        serial: Int!
+        tech_stack: String
+        tech_stack_exp: String
+        tech_last_used: String
+        developer_id: Int   
+    }
 
+    type Project{
+        serial:Int!
+        developer_id:Int
+        proj_title: String
+        proj_desc: String
+        proj_type: String
+    }
 
     input signinDeveloperUserInput{
         developer_email: String
         developer_password: String
     }
-
-    input signinDevInput {
-        developer_fname: String!
-        developer_mname: String
-        developer_lname: String!
-        developer_high_qualification: String
-        developer_phone: String!
-        developer_email: String!
-        developer_password: String!
-        developer_country: String!
-        developer_tech1: String!
-        developer_tech2: String
-        developer_tech3: String
-        developer_tech1_exp: String!
-        developer_tech2_exp: String
-        developer_tech3_exp: String
-        developer_resume: Upload
-        developer_company1: String!
-        developer_company1_project: String!
-        developer_company1_start: Date!
-        developer_company2: String
-        developer_company2_start: Date
-        developer_company2_end: Date
-        developer_company2_project: String
-        developer_type: String!
-     }
-
     input DeveloperDetails{
-        developer_id: Int!
         developer_fname: String
         developer_mname: String
         developer_lname: String
@@ -77,74 +62,115 @@ const developerQueryTypesAndInputs = `
         developer_password: String
         developer_type: String
     }
+    input ExperienceDetails{
+        tech_stack: String
+        tech_stack_exp: String
+        tech_last_used: String
+        developer_id: Int
+    }
     
 `;
 
 const developerQuery = `
     getDeveloper:[Developer]
     getDeveloperUser: DeveloperUser
-    getDashboard(userId: Int!): Dashboard
+    getDashboard: Dashboard
+    getExperienceList: [Experience]
+    getExperienceById(serial: Int!): Experience!
+    getDeveloperProjectList: [Project]
 `;
 
 const developerMutation = `
-
-    signupDev(data:signinDevInput):String!
     signinDeveloper(data:signinDeveloperUserInput!):Token
     updateDeveloper(data:DeveloperDetails!):DeveloperUser
+    
+    createExperience(data:ExperienceDetails!):Experience
+    updateExperience(data:ExperienceDetails!):Experience
 
 `;
 
 
 const developerQueryResolvers = {
-
     getDeveloperUser: async (_, args, { userId }) => {
-        console.log(userId);
         const developer = await prisma.jmkdevinfo.findFirst({ where: { developer_id: userId } });
         if (!developer) throw new ForbiddenError('Developer not found')
         return developer;
     },
-
     getDeveloper: async (_, args, { userId }) => {
         if (!userId) throw new ForbiddenError('user need to login');
         const developerList = await prisma.jmkdevinfo.findMany();
         return developerList;
     },
-
     getDashboard: async (_, args, { userId }) => {
         if (!userId) throw new ForbiddenError('user need to login');
-        console.log('hitting');
+
+        const dashboardData = await prisma.$transaction([
+            prisma.jmkdevtechdet.findMany({
+                where: {
+                    developer_id: userId,
+                },
+            }),
+            prisma.jmkdevprojdet.findMany({
+                where: {
+                    developer_id: userId,
+                },
+            }),
+        ]);
+
+        const [experienceList, projectList] = dashboardData;
+
+        const totalTechStackExp = experienceList.reduce(
+            (total, item) => total + parseInt(item.tech_stack_exp),
+            0
+        );
+
         const dashboard = {
-            experience: 3,
-            projects: 5
+            experience: totalTechStackExp,
+            projects: projectList.length,
         };
+
         return dashboard;
     },
+    getExperienceList: async (_, args, { userId }) => {
+        if (!userId) throw new ForbiddenError('user need to login');
 
+        const experienceList = await prisma.jmkdevtechdet.findMany({
+            where: {
+                developer_id: userId
+            }
+        });
+        experienceList.forEach((experience) => {
+            if (experience.tech_last_used) {
+                const timestamp = experience.tech_last_used.getTime();
+                experience.tech_last_used = new Date(timestamp).toLocaleDateString();
+            }
+        });
+        return experienceList;
+
+    },
+    getExperienceById: async (_, args, { userId, role }) => {
+        // if (!userId) throw new ForbiddenError('invalid token');
+        if (!args.serial) throw new ForbiddenError('serial is required !');
+        const experience = await prisma.jmkdevtechdet.findFirst({
+            where: {
+                serial: args.serial,
+            }
+        });
+        if (!experience) return new ApolloError('Experience does not exist!');
+        return experience;
+    },
+    getDeveloperProjectList: async (_, args, { userId, role }) => {
+        // if (!userId) throw new ForbiddenError('user need to login');
+
+        const projectList = await prisma.jmkdevprojdet.findMany({
+            where: {
+                developer_id: args.userId
+            }
+        });
+        return projectList;
+    }
 };
 const developerMutationResolver = {
-
-    signupDev: async (_, { data }) => {
-        const dev = await prisma.jmkdevinfo.findFirst({
-            where: { developer_email: data.developer_email },
-        })
-        if (dev)
-            throw new AuthenticationError('developer already exist with that email')
-        let file
-        if (data.developer_resume) {
-            file = await uploadImgToAWS(data.developer_resume, 'developer_resume/')
-            if (!file.data) throw new ApolloError('Someting went wrong !')
-        }
-        const newDev = await prisma.jmkdevinfo.create({
-            data: {
-                ...data,
-                developer_resume: file?.data?.Location ?? '',
-                developer_resume_key: file?.data?.key ?? '',
-            },
-        })
-        if (!newDev) throw new AuthenticationError('Invalid input')
-        return 'success'
-    },
-
     signinDeveloper: async (_, { data }) => {
         const developer = await prisma.jmkdevinfo.findFirstOrThrow({
             where: {
@@ -157,18 +183,41 @@ const developerMutationResolver = {
         const token = jwt.sign({ userId: developer.developer_id, role: ROLE }, process.env.JWT_SECRET_KEY)
         return { token };
     },
+    updateDeveloper: async (_, { data }, { userId }) => {
+        try {
+            console.log(userId);
+            const updatedDeveloper = await prisma.jmkdevinfo.update({
+                where: { developer_id: userId },
+                data: { ...data },
+            });
 
-    updateDeveloper: async (_, { data }) => {
-        const { developer_id, ...updatedData } = data;
-
-        const updatedDeveloper = await prisma.jmkdevinfo.update({
-            where: { developer_id },
-            data: updatedData,
-        });
-
-        if (!updatedDeveloper) throw new ForbiddenError('Developer not found');
-        console.log(updatedData);
-        return updatedDeveloper;
+            if (!updatedDeveloper) throw new ForbiddenError('Developer not found');
+            return updatedDeveloper;
+        } catch (error) {
+            console.log(error);
+            return new ForbiddenError("Error Occured");
+        }
+    },
+    createExperience: async (_, { data }) => {
+        try {
+            const techLastUsed = data.tech_last_used ? new Date(data.tech_last_used) : null;
+            const newExperience = await prisma.jmkdevtechdet.create({
+                data: {
+                    tech_stack: data.tech_stack,
+                    tech_stack_exp: data.tech_stack_exp,
+                    tech_last_used: techLastUsed,
+                    developer_id: data.developer_id,
+                },
+            });
+            return newExperience;
+        } catch (error) {
+            console.log(error);
+            throw new ForbiddenError('Error occured!!!');
+        }
+    }
+    ,
+    updateExperience: async (_, { data }) => {
+        const { developer_id, ..._updatedData } = data;
     }
 
 }
