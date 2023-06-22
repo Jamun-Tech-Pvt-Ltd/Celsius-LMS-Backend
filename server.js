1ba108d89e3f11ad4984b7f41fcb7083f136039e
sarthak2057 <shrutikc59@gmail.com>
2023-06-22 09:43:10 +0545

    loggeradded

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs'
 import express from 'express'
 import typeDefs from './typeDefs.js'
 import resolvers from './resolvers.js'
-import bodyParser from 'body-parser'
 import jwt from 'jsonwebtoken'
-import morgan from 'morgan'
-import IP from 'ip'
 import { PrismaClient } from '@prisma/client'
 import logger from './utils/logger.js'
 
@@ -53,28 +50,6 @@ async function startServer() {
   app.use(express.json())
   app.use(graphqlUploadExpress())
 
-  // morgan.token('id-role', (req, res) => {
-  //   const { authorization } = req.headers
-  //   if (authorization) {
-  //     const { userId, role } = jwt.verify(
-  //       authorization,
-  //       process.env.JWT_SECRET_KEY
-  //     )
-  //     if (userId && role) return `${userId} ${role}`
-  //   } else {
-  //     return 'Guest'
-  //   }
-  // })
-  // morgan.token('ip-address', (req, res) => {
-  //   const ip_address = IP.address()
-  //   return ip_address
-  // })
-  // morgan.token('graphql-query', (req) => {
-  //   const { query, variables, operationName } = req.body
-  //   return ` ${operationName}`
-  // })
-
-  // app.use(morgan(':ip-address :id-role :graphql-query  :status [:date[web]]'))
   app.use(logger)
 
   server.applyMiddleware({ app })
