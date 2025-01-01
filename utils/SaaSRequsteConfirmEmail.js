b961a1265d09de502b7c07ef99a3003be887898e
pratek7 <pratikjungkarki123@gmail.com>
2025-01-01 11:28:45 +0545

    fix

--- utils/SaaSRequsteConfirmEmail.js
+++ utils/SaaSRequsteConfirmEmail.js
@@ -21,13 +21,14 @@ const saaSRequsteConfirmEmail = (c_name, c_email, c_username, c_password) => `
       <img
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/topVector.png"
         alt="vector"
+         style='width:200px; height:200px;'
       />
       <img
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/rightLine.png"
 
         alt="vector"
         class="right__line"
-        style="width: 456px; position: absolute; right: 0%"
+ style="width: 156px; position: absolute; right: 0%"
       />
 
       <div class="content" style="width: 470px; margin: 0 auto; display: flex; align-items: center; flex-direction: column;">
@@ -35,7 +36,7 @@ const saaSRequsteConfirmEmail = (c_name, c_email, c_username, c_password) => `
           src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/logo.png"
           alt="logo"
           class="logo"
-          style="width: 354px; height: 75px"
+  style="width: 354px; height: 75px"
         />
 
         <h1
@@ -76,14 +77,14 @@ const saaSRequsteConfirmEmail = (c_name, c_email, c_username, c_password) => `
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/leftLine.png"
         alt="vector"
         class="left__line"
-        style="width: 500px; position: absolute; bottom: 0; left: 0"
+ style="width: 100px; position: absolute; bottom: 0; left: 0"
       />
 
       <img
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/vector_1.png"
         alt=""
         class="btn__vector"
-        style="position: absolute; width: 290px; right: 0; bottom: -5rem"
+style="position: absolute; width: 290px; right: 0; bottom: -5rem"
       />
     </div>
   </body>
--- utils/SaaSRequsteEmail.js
+++ utils/SaaSRequsteEmail.js
@@ -21,12 +21,13 @@ const saaSRequsteEmail = (c_name, c_email, c_package, c_package_type) => `
       <img
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/topVector.png"
         alt="vector"
+         style='width:200px; height:200px;'
       />
       <img
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/rightLine.png"
         alt="vector"
         class="right__line"
-        style="width: 456px; position: absolute; right: 0%"
+ style="width: 156px; position: absolute; right: 0%"
       />
 
       <div class="content" style="width: 470px; margin: 0 auto; display: flex; align-items: center; flex-direction: column;">
@@ -34,7 +35,7 @@ const saaSRequsteEmail = (c_name, c_email, c_package, c_package_type) => `
           src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/logo.png"
           alt="logo"
           class="logo"
-          style="width: 354px; height: 75px"
+ style="width: 354px; height: 75px"
         />
 
         <h1
@@ -75,14 +76,14 @@ const saaSRequsteEmail = (c_name, c_email, c_package, c_package_type) => `
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/leftLine.png"
         alt="vector"
         class="left__line"
-        style="width: 500px; position: absolute; bottom: 0; left: 0"
+          style="width: 100px; position: absolute; bottom: 0; left: 0"
       />
 
       <img
         src="https://jmkcrsmn.s3.us-east-2.amazonaws.com/email_teamplate/vector_1.png"
         alt=""
         class="btn__vector"
-        style="position: absolute; width: 290px; right: 0; bottom: -5rem"
+style="position: absolute; width: 290px; right: 0; bottom: -5rem"
       />
     </div>
   </body>
