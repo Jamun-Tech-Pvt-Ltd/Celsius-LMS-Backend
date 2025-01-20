fff3b9be417d0ebd59f4ef9240c92090e819d09e
nabin <nabin0433@gmail.com>
2025-01-20 21:55:57 +0545

    update

--- controller/admin/index.js
+++ controller/admin/index.js
@@ -1912,7 +1912,7 @@ const adminResolvers = {
         where: { company_id: admin.company_id },
       });
 
-      if (existingCertLayout.serial) {
+      if (existingCertLayout?.serial) {
         if (data.logo) {
           await deleteImgToAWS(existingCertLayout.logo_key);
           const uploadedLogo = await uploadImgToAWS(data.logo, 'cert_logo/');
